4f153d8446aa6145cb08791457059daa5f93156b
CMcCown <connermccown94@gmail.com>
2022-02-04 16:23:44 -0700

    Added high score local.Storage

--- assets/js/game.js
+++ assets/js/game.js
@@ -131,6 +131,22 @@ var startGame = function () {
 var endGame = function() {
   window.alert("The game has now ended. Let's see how you did!");
 
+  //check localstorage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem("Highscore:");
+  if(highScore === null){
+    highScore = 0;
+  }
+  //if player has more money than the high score, player has a new high score
+  if(playerInfo.money > highScore){
+    localStorage.setItem("Highscore:", playerInfo.money);
+    localStorage.setItem("Name", playerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+  }
+  else{
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+  }
+
   //if player is still alive, player wins
   if (playerInfo.health > 0) {
     window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
