c366c48e22f7f34482d97ad41da2cb0f19ebdb4b
CMcCown <connermccown94@gmail.com>
2022-02-04 13:56:25 -0700

    Added getPlayerName function to correct player name issue

--- assets/js/game.js
+++ assets/js/game.js
@@ -166,8 +166,18 @@ var endGame = function() {
    return value;
  };
 
+var getPlayerName = function() {
+  var name = "";
+
+  while (name==="" || name === null){
+    name = prompt("What is your robot's name?");
+  }
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
  var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
